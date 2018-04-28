b1d78857f3156cd9410366a1edad0e33234a93fd
Pablo <serna.pablo95@gmail.com>
2018-04-28 13:48:45 +0200

    Primera version con notificaciones

---
+++ app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -0,0 +1,15 @@
+package com.app.androidkt.mqtt;
+
+/**
+ * Created by brijesh on 20/4/17.
+ */
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://172.20.10.9:1883";
+
+    public static final String PUBLISH_TOPIC = "untopicguapo";
+
+    public static final String CLIENT_ID = "Nosaltres";
+}
+
